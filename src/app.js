65f6608dd799ec8a6e574d4cddb5808bc0220c77
Zachary Chai <zachary.chai@outlook.com>
2018-12-24 16:09:15 -0500

    sign jwt

--- src/app.js
+++ src/app.js
@@ -9,4 +9,4 @@ app.use(BodyParser());
 app.use(router.routes())
 // app.use(router.allowedMethods())
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
--- src/routes/jwtsRouter.js
+++ src/routes/jwtsRouter.js
@@ -1,4 +1,5 @@
 const Router = require('koa-router');
+const jwt = require('jsonwebtoken');
 
 const router = new Router();
 
@@ -6,8 +7,39 @@ router.get('/api/jwts', async (ctx) => {
     ctx.body = 'Hello JWTs'
 });
 
-router.post('/api/jwts', async (ctx) => {
-    ctx.body = ctx.request.body
+router.post('/api/jwts/sign', async (ctx) => {
+  if (ctx.request.body == null || Object.keys(ctx.request.body).length === 0) {
+    ctx.throw(422)
+  }
+  console.log(ctx.request.body)
+  const data = ctx.request.body.data
+  let jwtOptions = {
+      algorithm: data.alg,
+
+      issuer: data.iss,
+      audience: data.aud
+  }
+
+  if (data.sub != null) {
+    jwtOptions.subject = data.sub
+  }
+  if (data.kid != null) {
+    jwtOptions.keyid = data.kid
+  }
+
+  let jwtPayload = data.payload
+  jwtPayload.exp = data.exp
+  if (data.jti != null) {
+    jwtPayload.jwtid = data.jti
+  }
+
+  let jwtKey = data.key
+
+  let token = jwt.sign(jwtPayload, jwtKey, jwtOptions)
+
+  ctx.body = {
+    data: token
+  }
 });
 
 module.exports = router;
